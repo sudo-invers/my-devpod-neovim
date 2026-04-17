ab121e7b9c541887106e4589ccc0780b132bcdf3
Dhonatan <dhonatanjose8@gmail.com>
2026-04-17 16:03:51 -0300

    Now install neovim without a devoontainer
    need a pre dev container too work now

--- setup.sh
+++ setup.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/env bash
-
 set -e
+
+# Installing nvim...
+if ! command -v nvim &> /dev/null; then
+    echo "[*] Neovim não encontrado. Baixando e instalando (requer sudo)..."
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+    sudo tar -C /opt -xzf nvim-linux64.tar.gz
+    sudo ln -sf /opt/nvim-linux64/bin/nvim /usr/local/bin/nvim
+    rm nvim-linux64.tar.gz
+fi
 
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
 
-echo "[*] Installing AstroNvim..."
+echo "[*] Configuring AstroNvim..."
 
 if [ -d "$NVIM_CONFIG_DIR" ]; then
-    echo "[!] Diretório $NVIM_CONFIG_DIR já existe. Removendo para instalação limpa..."
+    echo "[!] Directory $NVIM_CONFIG_DIR already exist. Removing for a clear installation...
     rm -rf "$NVIM_CONFIG_DIR"
 fi
 
 # Clone AstroNvim
 git clone --depth 1 https://github.com/AstroNvim/template "$NVIM_CONFIG_DIR"
-
 rm -rf "$NVIM_CONFIG_DIR/.git"
 
-echo "[*] Instalação do AstroNvim concluída com sucesso!"
+echo "[*] Instalation is sucessuful!"
+sleep 1
+exit 0
